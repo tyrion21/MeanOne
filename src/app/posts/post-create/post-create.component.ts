11a7dbdf81df57ec59e6d681b32705f4fba6d9dd
Jason.Hermida <jhermidadiaz@gmail.com>
2019-04-05 15:49:14 -0300

    commit 5

--- src/app/posts/post-create/post-create.component.ts
+++ src/app/posts/post-create/post-create.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { EventEmitter, Output } from '@angular/core';
-import { Post } from '../post.model';
 import { NgForm } from '@angular/forms';
-import { ReturnStatement } from '@angular/compiler';
+import { PostsServices } from '../posts.service';
 
 @Component({
   selector: 'app-post-create',
@@ -12,20 +10,17 @@ import { ReturnStatement } from '@angular/compiler';
 export class PostCreateComponent implements OnInit {
   enteredContent = '';
   enteredTitle = '';
-  @Output() postCreated = new EventEmitter<Post>();
 
-
-  constructor() { }
+  constructor(public postsService: PostsServices) { }
 
   ngOnInit() {
   }
 
-  onAddPost(form: NgForm){
-    if(form.invalid){
+  onAddPost(form: NgForm) {
+    if (form.invalid) {
       return;
     }
-    const post: Post = { title: form.value.title,
-                  content: form.value.content};
-    this.postCreated.emit(post);
+    this.postsService.addPost(form.value.title, form.value.content);
+    form.resetForm();
   }
 }
--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,18 +1,34 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
+
 import { Post } from '../post.model';
+import { PostsServices } from '../posts.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css']
 })
-export class PostListComponent implements OnInit {
+export class PostListComponent implements OnInit, OnDestroy {
 
-  @Input() posts: Post[] = [];
+  posts: Post[] = [];
 
-  constructor() { }
+  private postsSub: Subscription;
+
+  constructor(public postsService: PostsServices) {
+
+   }
 
   ngOnInit() {
+    this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostUpdateListener().
+      subscribe((posts: Post[]) => {
+        this.posts = posts;
+      });
+  }
+
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
   }
 
   // posts = [
